5491f85c709c2c853b768dadfcc45f37493827df
cipriandinu01 <cipriandinu01@gmail.com>
2025-06-04 10:50:42 +0000

    plm

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "Ciprian" {
+  name     = "ciprian1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "Valentin" {
+  name     = "Valentin1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "Enache" {
+  name     = "Enache1"
+  location = "West Europe"
+}
